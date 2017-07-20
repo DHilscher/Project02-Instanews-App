6109b90b58a9bb294560b7730048e9764e377b05
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-20 14:59:50 -0700

    files/folders added

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp'),
+    prettyError = require('gulp-prettyerror'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create(),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano');
 
 gulp.task('scripts', ['eslint'], function(){
     gulp.src('./js/*.js')
@@ -25,6 +29,7 @@ gulp.task('eslint', () => {
 
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('sass/*.scss', ['sass']);
 });
 
 gulp.task('browser-sync', function() {
@@ -33,7 +38,20 @@ gulp.task('browser-sync', function() {
             baseDir: "./"
         }
     });
-    gulp.watch('build/js/*.js').on('change', browserSync.reload);
+    gulp.watch(['build/js/*.js','build/css/*.css']).on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(prettyError())
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('default', ['watch','browser-sync']);
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready( function(){
-var david = 'david';
-console.log(david);
+
 });
 
