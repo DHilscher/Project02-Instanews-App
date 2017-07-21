25cd90bd74b40adc77f630512654eff55bcee4bf
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-21 13:31:35 -0700

    logo added/background-color

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-$(document).ready( function(){
-
+$('button').on('click', function() {
+    $.getJSON( "" )
+  .done(function(data) {
+    $.each(data, function( key, value ){
+      $('.results').append(data.);
+      /* Append your list items here */
+    });
+  });
 });
-
