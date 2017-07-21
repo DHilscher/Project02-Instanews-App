9e31871ffad0d520b37a440a1951fee6d15c5df3
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-21 16:46:22 -0700

    ajax working

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-$('button').on('click', function() {
-    $.getJSON( "" )
-  .done(function(data) {
-    $.each(data, function( key, value ){
-      $('.results').append(data.);
-      /* Append your list items here */
+$(function() {
+    $('.drop-down').on('change', function() {  
+        var stories = $(this).val();
+        var url = 'https://api.nytimes.com/svc/topstories/v2/' + stories + '.json';
+        console.log(url);
+        
+        url += '?' + $.param({
+        'api-key': "1dea2f5cd03e4217bc34ba95f65d5c90"
+        });
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(data) {
+            $.each(data.results,function(key,value){
+            $(".stories").append()
+        }).fail(function(err) {
+            throw err;
+        });
     });
-  });
 });
+
+
+
+
